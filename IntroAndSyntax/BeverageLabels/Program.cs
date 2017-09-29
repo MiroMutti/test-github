4b7dcfa81d5e14129c7bc87ae069255ffa057e71
miromutti <cxcvjis@sjd.com>
2017-09-29 22:40:03 +0300

    Added first homework to repo

---
+++ IntroAndSyntax/BeverageLabels/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            int energyPerHundred = int.Parse(Console.ReadLine());
+            int sugarPerHundred = int.Parse(Console.ReadLine());
+
+            double energy = volume * energyPerHundred / 100.00;
+            double sugar = volume * sugarPerHundred / 100.00;
+
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars", energy, sugar);
+        }
+    }
+}
---
+++ IntroAndSyntax/DebitCardNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstDigits = int.Parse(Console.ReadLine());
+            int secondDigits = int.Parse(Console.ReadLine());
+            int thirdDigits = int.Parse(Console.ReadLine());
+            int fourthDigits = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:0000} {1:0000} {2:0000} {3:0000}", firstDigits, secondDigits, thirdDigits, fourthDigits);
+        }
+    }
+}
